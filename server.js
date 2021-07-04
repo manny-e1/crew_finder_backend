0f4bfa3b50dd07e013be73f4e96e5151758c3fbd
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-07-04 13:05:00 +0300

    feat(middlewares): folder structure updated

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import http from 'http';
 
 const PORT = process.env.PORT || 5000;
 
-http.createServer(app).listen(PORT, () => console.log()`listening on port ${PORT}`);
+http.createServer(app).listen(PORT, () => console.log(`listening on port ${PORT}`);
 
