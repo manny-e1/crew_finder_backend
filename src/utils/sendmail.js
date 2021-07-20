33fe1e382b15b9aa332bb35fc628bae0a57c788d
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-07-20 08:51:28 +0300

    feat: email functionality

---
+++ src/utils/sendmail.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = (obj) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"Crew Finder" ${process.env.EMAIL_SENDER}`,
+    to: obj.to,
+    subject: obj.subject,
+    html: obj.text,
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+ 
+
