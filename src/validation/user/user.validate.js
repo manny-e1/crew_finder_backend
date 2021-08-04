e8fe10c169c716cad8d9b253ddc711a6d3f16358
Fikir <fikirmuluken@gmail.com>
2021-08-04 16:46:59 +0300

    feat:add validation to some attributes

--- src/validation/user/user.validate.js
+++ src/validation/user/user.validate.js
@@ -10,4 +10,16 @@ const validateUser = [
         .withMessage('Username should be string')
         .isLength({max: 6})
         .withMessage('Length of Username should be less than 6'),
+    body('email')
+        .isEmail()
+        .normalizeEmail()
+        .toLowerCase()
+        .withMessage('Provide a valid email'),
+    body('password')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i")
+        .withMessage('Password should be minimum of 8 characters and include an Uppercase, a lowercase, a number and a character'),
+    body('isActive')
+        .isBoolean()
+        .withMessage('isActive should be a boolean'),
+
 ];
