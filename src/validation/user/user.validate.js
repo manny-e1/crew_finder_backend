9dfd78e91946b80c932650c99fff316270f3ce43
Fikir <fikirmuluken@gmail.com>
2021-08-04 16:45:37 +0300

    add validation to some attributes

--- src/validation/user/user.validate.js
+++ src/validation/user/user.validate.js
@@ -1,6 +1,13 @@
 import { body } from 'express-validator';
 
 const validateUser = [
-    body('fullName'),
-    body('username'),
+    body('fullName')
+        .isString()
+        .trim()
+        .withMessage('Full name is need to be a String'),
+    body('username')
+        .isString()
+        .withMessage('Username should be string')
+        .isLength({max: 6})
+        .withMessage('Length of Username should be less than 6'),
 ];
