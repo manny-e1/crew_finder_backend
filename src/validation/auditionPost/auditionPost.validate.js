3328c4022ba518fac2d258e91d3adfa2230939b3
Fikir <fikirmuluken@gmail.com>
2021-08-07 17:02:06 +0300

    feat:add validation to the rest attributes

--- src/validation/auditionPost/auditionPost.validate.js
+++ src/validation/auditionPost/auditionPost.validate.js
@@ -1,4 +1,5 @@
 import { body } from 'express-validator';
+import { TALENT } from '../../constants/enums.constants.js';
 
 const validateAuditionPost = [
     body('text')
@@ -6,6 +7,11 @@ const validateAuditionPost = [
         .withMessage('Text of Audition Post should be String!')
         .isLength({min: 10, max: 200})
         .withMessage('Audition Post text should range 10 - 200 characters!'),
+    body('talents')
+        .isString()
+        .withMessage('Talent of Audition Post should be String!')
+        .isIn(Object.values(TALENT))
+        .withMessage('Talent is not found!'),
     body('isAcceptingApplication')
         .isBoolean()
         .withMessage('isAcceptingApplication should be a boolean'),
