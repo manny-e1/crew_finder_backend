85eb0e5a29f31b695c912479b43d9622f8f3aa33
Fikir <fikirmuluken@gmail.com>
2021-08-09 17:21:48 +0300

    feat:hook applicationValidate with its controller and route

--- src/routes/application.routes.js
+++ src/routes/application.routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { httpCreateApplication, httpDeleteApplication, httpGetApplication, httpGetApplications, httpUpdateApplication } from '../controllers/application.controllers.js';
 import { errorCatcher } from '../middlewares/error.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated..js';
+import validateApplicationLetter from '../validation/application/application.validate.js';
 
 const router = Router();
 
 router.route('/')
         .post(
+            validateApplicationLetter,
             errorCatcher(isAuthenticated),
             errorCatcher(httpCreateApplication)
         )
