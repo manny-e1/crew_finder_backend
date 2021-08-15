f727e03e363dc22f09783d11b17e4693781cbaa4
Fikir <fikirmuluken@gmail.com>
2021-08-15 19:44:02 +0300

    fix:format the user.test.js file

--- src/controllers/user/user.test.js
+++ src/controllers/user/user.test.js
@@ -1,105 +1,98 @@
 import request from "supertest";
-import app from '../../app';
+import app from "../../app";
 
+describe("Users API", () => {
+  describe("Test GET /users", () => {
+    test("it should respond with 200 success", async () => {
+      const response = await request(app)
+        .get("/users")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
 
+  describe("Test POST /users", () => {
+    const completeUserData = {
+      fullName: "Test Name",
+      username: "tUser6",
+      email: "test6@test.com",
+      password: "Test123@",
+      role: "ADMIN",
+      talent: "ACTOR",
+      otherTalents: "DIRECTOR",
+      verification: "FAMOUS",
+      birthdate: "2000-01-01",
+      gender: "MALE",
+      address: {
+        country: "Test country",
+        city: "Test city",
+      },
+      phoneNumber: "25190000000",
+    };
 
-describe('Users API', () =>{
-    describe('Test GET /users', () => {
-    test('it should respond with 200 success', async ()=>{
-        const response = await request(app)            
-            .get('/users')
-            .expect('Content-Type', /json/)
-            .expect(200);
-        });
+    const userDataWithoutBirthDate = {
+      fullName: "Test Name",
+      username: "tUser",
+      email: "test@test.com",
+      password: "Test123@",
+      role: "ADMIN",
+      talent: "ACTOR",
+      otherTalents: "DIRECTOR",
+      verification: "FAMOUS",
+      gender: "MALE",
+      address: {
+        country: "Test country",
+        city: "Test city",
+      },
+      phoneNumber: "25190000000",
+    };
+
+    const invalidUserData = {
+      fullName: "Test Name",
+      username: "tUser5",
+      email: "test5@test.com",
+      password: "Test123@",
+      role: "ADMIN",
+      talent: "test here",
+      otherTalents: "DIRECTOR",
+      verification: "FAMOUS",
+      birthdate: "2000-01-01",
+      gender: "MALE",
+      address: {
+        country: "Test country",
+        city: "Test city",
+      },
+      phoneNumber: "25190000000",
+    };
+
+    test("it should respond with 201 created", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send(completeUserData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+
+      // const requestDate = new Date(completeUserData.birthdate).valueOf();
+      // const responseDate = new Date(response.body.birthdate).valueOf();
+
+      // expect(responseDate).toBe(requestDate);
+      // expect(response.body).toMatchObject(userDataWithoutBirthDate);
     });
 
-    describe('Test POST /users', () => {
-
-        const completeUserData = {
-            fullName: "Test Name",
-            username: "tUser6",
-            email: "test6@test.com",
-            password: "Test123@",
-            role: "ADMIN",
-            talent: "ACTOR",
-            otherTalents: "DIRECTOR",
-            verification: "FAMOUS",
-            birthdate: "2000-01-01",
-            gender: "MALE",
-            address: {
-                country: "Test country",
-                city: "Test city",
-            },
-            phoneNumber: "25190000000",
-        };
-
-        const userDataWithoutBirthDate = {
-            fullName: "Test Name",
-            username: "tUser",
-            email: "test@test.com",
-            password: "Test123@",
-            role: "ADMIN",
-            talent: "ACTOR",
-            otherTalents: "DIRECTOR",
-            verification: "FAMOUS",
-            gender: "MALE",
-            address: {
-                country: "Test country",
-                city: "Test city",
-            },
-            phoneNumber: "25190000000",
-        };
-
-        const invalidUserData = {
-            fullName: "Test Name",
-            username: "tUser5",
-            email: "test5@test.com",
-            password: "Test123@",
-            role: "ADMIN",
-            talent: "test here",
-            otherTalents: "DIRECTOR",
-            verification: "FAMOUS",
-            birthdate: "2000-01-01",
-            gender: "MALE",
-            address: {
-                country: "Test country",
-                city: "Test city",
-            },
-            phoneNumber: "25190000000",
-        };
-
-        
-        test('it should respond with 201 created', async () => {
-            const response = await request(app)
-                .post('/users')
-                .send(completeUserData)
-                .expect('Content-Type', /json/)
-                .expect(201);
-
-            // const requestDate = new Date(completeUserData.birthdate).valueOf();
-            // const responseDate = new Date(response.body.birthdate).valueOf();
-
-            // expect(responseDate).toBe(requestDate);
-            // expect(response.body).toMatchObject(userDataWithoutBirthDate);
-        });
-
-        test('it should respond with 400 bad request, catch missing required field', async () => {
-            const response = await request(app)
-                .post('/users')
-                .send(userDataWithoutBirthDate)
-                .expect('Content-Type', /json/)
-                .expect(400);
-        });
-
-        test('it should respond with 400, when invalid data is given', async () => {
-            const response = await request(app)
-                .post('/users')
-                .send(invalidUserData)
-                .expect('Content-Type', /json/)
-                .expect(400);
-        });
-
-        
+    test("it should respond with 400 bad request, catch missing required field", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send(userDataWithoutBirthDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
     });
 
+    test("it should respond with 400, when invalid data is given", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send(invalidUserData)
+        .expect("Content-Type", /json/)
+        .expect(400);
+    });
+  });
 });
