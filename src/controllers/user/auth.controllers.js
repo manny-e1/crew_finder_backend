b2c34145ee9a45b9b8cae9cf8b09fd1ce3760c8c
Fikir <fikirmuluken@gmail.com>
2021-08-15 16:23:21 +0300

    fix:minor improvement

--- src/controllers/user/auth.controllers.js
+++ src/controllers/user/auth.controllers.js
@@ -18,7 +18,6 @@ async function httpCreateUser(req,res){
     await createUser(req.body, req.headers.host);
     return res
             .status(201)
-            .json(req.body)
             .json("success");
 
 }
