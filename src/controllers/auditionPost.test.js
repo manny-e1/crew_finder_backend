68fe87387691e77ba55faa0391a522def2b52596
Fikir <fikirmuluken@gmail.com>
2021-08-17 23:51:53 +0300

    feat:test code for updating audition

--- src/controllers/auditionPost.test.js
+++ src/controllers/auditionPost.test.js
@@ -13,11 +13,31 @@ describe('Audition Post API', () => {
         });
     });
 
+    describe('Test GET /auditionPosts/:id', () => {
+        test('it should respond with 200 after fetching the specified audition post', async () => {
+            const _id = '611bfd157abd7837d81b5a28';
+            const response = await request(app)
+                .get(`/auditionposts/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMwY2YwMDY3ZGU1MmZiMDMzOThmNCIsImlhdCI6MTYyOTIyODI4Nn0.N6cxvsjse9eJrelrby-sa_aN-xIrRUupJZYLd95xpWI')
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+
+        test('it should respond with 404 if the specified audition post does not exist', async () => {
+            const _id = '611bfd157abd7837d81b5b28';
+            const response = await request(app)
+                .get(`/auditionposts/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMwY2YwMDY3ZGU1MmZiMDMzOThmNCIsImlhdCI6MTYyOTIyODI4Nn0.N6cxvsjse9eJrelrby-sa_aN-xIrRUupJZYLd95xpWI')
+                .expect('Content-Type', /json/)
+                .expect(404);
+        });
+    });
+
     describe('Test POST /auditionposts', () => {
         const completeAuditionPost = {
-            "title": "Test Audition",
-            "text": "Testing the audition post here",
-            "talents": ["ACTOR"],
+            title: "Test Audition",
+            text: "Testing the audition post here",
+            talents: ["ACTOR"],
         };
 
         const incompleteAuditionPost = {
@@ -42,6 +62,45 @@ describe('Audition Post API', () => {
                 .send(incompleteAuditionPost)
                 .expect('Content-Type', /json/)
                 .expect(400);
+        });
+    });
+
+    describe('Test PUT /auditionposts/:id', () => {
+        const toUpdateAuditionPost = {
+            author: "611c0cf0067de52fb03398f4",
+            title: "Update Test Audition",
+            text: "Testing the audition post here",
+            talents: ["ACTOR"],
+        };
+
+        test('it should respond with 200 after updating existing audition post', async () => {
+            const _id = '611c1d988c08472490c5228a';
+            const response = await request(app)
+                .put(`/auditionposts/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMwY2YwMDY3ZGU1MmZiMDMzOThmNCIsImlhdCI6MTYyOTIyODI4Nn0.N6cxvsjse9eJrelrby-sa_aN-xIrRUupJZYLd95xpWI')
+                .send(toUpdateAuditionPost)
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+
+        test('it should respond with 403 when trying to update other\'s audition', async () => {
+            const _id = '611c2012ddddd63e6879b1fb';
+            const response = await request(app)
+                .put(`/auditionposts/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMwY2YwMDY3ZGU1MmZiMDMzOThmNCIsImlhdCI6MTYyOTIyODI4Nn0.N6cxvsjse9eJrelrby-sa_aN-xIrRUupJZYLd95xpWI')
+                .send(toUpdateAuditionPost)
+                .expect('Content-Type', /json/)
+                .expect(403);
+        });
+
+        test('it should respond with 404 when trying to update unExisting audition', async () => {
+            const _id = '611c2012ddddd63e6879b1eb';
+            const response = await request(app)
+                .put(`/auditionposts/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMwY2YwMDY3ZGU1MmZiMDMzOThmNCIsImlhdCI6MTYyOTIyODI4Nn0.N6cxvsjse9eJrelrby-sa_aN-xIrRUupJZYLd95xpWI')
+                .send(toUpdateAuditionPost)
+                .expect('Content-Type', /json/)
+                .expect(404);
         });
     });
 });
