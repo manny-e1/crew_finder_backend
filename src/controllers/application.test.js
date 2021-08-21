9bdcb08ab9a9b23aec90784ba350b8643194920c
Fikir <fikirmuluken@gmail.com>
2021-08-21 21:56:56 +0300

    feat:test code for GET application with specific ID

--- src/controllers/application.test.js
+++ src/controllers/application.test.js
@@ -1,3 +1,4 @@
+import { describe } from '@jest/globals';
 import request from 'supertest';
 import app from '../app';
 
@@ -52,8 +53,29 @@ describe('Application API', () => {
         test('it should respond with 200 when fetching applications', async () =>{
             const response = request(app)
                 .get('/applications')
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
                 .expect('Content-Type',/json/)
                 .expect(200);
+        });
+    });
+
+    describe('Test GET /applications/id', () => {
+        test('it should respond with 200 if the application exists', async () => {
+            const _id = '61214461b53c90393c3cbf36';
+            const response = await request(app)
+                .get(`/applications/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
+                .expect('Content-Type',/json/)
+                .expect(200);
+        });
+
+        test('it should respond with 404 if the application does not exist', async () => {
+            const _id = '61214461b53c90393c3cbf44';
+            const response = await request(app)
+                .get(`/applications/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
+                .expect('Content-Type',/json/)
+                .expect(404);
         });
     });
 });
