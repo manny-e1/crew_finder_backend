66c6c4cbbf61dfead0529f4a7c4505b8e8fe5b2b
Fikir <fikirmuluken@gmail.com>
2021-08-21 23:18:11 +0300

    feat:test code for PUT application with specific ID

--- src/controllers/application.test.js
+++ src/controllers/application.test.js
@@ -5,7 +5,7 @@ import app from '../app';
 describe('Application API', () => {
     describe('Test POST /applications', () => {
         const applicationSchema = {
-            auditionPostId: '611c20a563ea184798a36507',
+            auditionPostId: '611c1dd5dd6bca1f38e0690b',
             applicantId: '611c1fdaddddd63e6879b1f7',
             applicationLetter: 'This a test application letter to a test auditionPost'
         };
@@ -76,6 +76,39 @@ describe('Application API', () => {
                 .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
                 .expect('Content-Type',/json/)
                 .expect(404);
+        });
+    });
+
+    describe('Test PUT /applications/id', () => {
+        const updateApplicationSchema = {
+            auditionPostId: '611c20a563ea184798a36507',
+            applicantId: '611c1fdaddddd63e6879b1f7',
+            applicationLetter: 'This a test to update the application letter'
+        };
+
+        const oUpdateApplicationSchema = {
+            auditionPostId: '611c20a563ea184798a36507',
+            applicantId: '611c1fdaddddd63e6879b122',
+            applicationLetter: 'This a test to update the application letter'
+        };
+        test('it should respond with 200 after updating the application', async () => {
+            const _id = '61215e4d0d95f31b30db2eee';
+            const response = await request(app)
+                .put(`/applications/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
+                .send(updateApplicationSchema)
+                .expect('Content-Type',/json/)
+                .expect(200);
+        });
+
+        test('it should respond with 403 when trying to update other\'s application', async () => {
+            const _id = '61214461b53c90393c3cbf36';
+            const response = await request(app)
+                .put(`/applications/${_id}`)
+                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMWMxZmRhZGRkZGQ2M2U2ODc5YjFmNyIsImlhdCI6MTYyOTU2OTg5MH0.RZyU8NgW6yOladmfYRzvUhrq5bA7eZVgG0o8M7s9Efo')
+                .send(oUpdateApplicationSchema)
+                .expect('Content-Type',/json/)
+                .expect(403);
         });
     });
 });
