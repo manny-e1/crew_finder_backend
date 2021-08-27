af5c448debe256dca1fa834ea3d58f2d20e46026
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-08-27 12:24:54 +0300

    feat: endorsement's get services

---
+++ src/services/endorsement.services.js
@@ -0,0 +1,24 @@
+import EndorsementModel from "../models/endorsement.model.js";
+
+async function endorseUser(endorserId, endorseeId) {
+  return EndorsementModel.create({
+    endorserId,
+    endorseeId
+  });
+}
+
+async function getAllEndorsements() {
+  return EndorsementModel.find();
+}
+
+async function getEndorsementByID(id) {
+  return EndorsementModel.findById(id);
+}
+
+async function getGivenEndorsements(endorserId) {
+  return EndorsementModel.find({endorserId});
+}
+
+async function getReceivedEndorsements(endorseeId) {
+  return EndorsementModel.find({endorseeId});
+}
