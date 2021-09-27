605c55e6820e688a57b5315f9dbebf1dae5c2e9e
fikir-2411 <fikirmuluken@gmail.com>
2021-09-28 00:48:04 +0300

    feat: add test code to check hashToken hashes the given token inside generateToken

--- src/utils/generateToken.test.js
+++ src/utils/generateToken.test.js
@@ -1,7 +1,14 @@
-import { generateToken } from "./generateToken";
+import { generateToken, hashToken } from "./generateToken";
+import crypto from 'crypto';
 
 test('should generate Token', () => {
     expect(generateToken()).toHaveProperty("expiration");
     expect(generateToken()).toHaveProperty("hashedToken");
     expect(generateToken()).toHaveProperty("token");
-})
+});
+
+test('should hash a token', () => {
+    const token = '1234567890';
+    const generated = crypto.createHash('sha256').update(token).digest('hex');
+    expect(hashToken(token)).toMatch(generated);
+});
