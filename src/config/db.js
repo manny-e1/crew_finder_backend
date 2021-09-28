5d5bf8619cfe569e87e6bddf39c9530bf7160dde
manny-e1 <aewnetu6@gmail.com>
2021-09-28 04:03:13 +0300

    feat: dsfd

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_DB_LOCAL, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-        })
-        console.log(`Database connected: ${connect.connection.host}`);
-    } catch (error) {
-        console.log(`${error}`);
-        process.exit(1)
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`${error}`);
+    process.exit(1);
+  }
+};
 
-export default connectDatabase
+export default connectDatabase;
